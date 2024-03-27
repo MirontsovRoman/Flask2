10191fd51b5df7b7a4792a1836ab0d13567fed9b
MirontsovRoman <roma419@mail.ru>
2024-03-27 13:01:55 +0300

    Add Homework

--- QuoteApi/ma_examples/schema.py
+++ QuoteApi/ma_examples/schema.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 
 class AuthorSchema(Schema):
@@ -8,3 +8,18 @@ class AuthorSchema(Schema):
     email = fields.Email(required=True, error_messages={
                        'required': 'email is required'})
     
+    
+class LearnerSchema(Schema):
+    uid = fields.Integer()
+    name = fields.Str(
+        required=True,
+        error_messages={
+            'required': {
+                'message': 'name is requided',
+                'code': 400
+            }
+        },
+        validate=[validate.Length(max=50)]
+    )
+    final_test = fields.Boolean(required=True, error_messages={
+        'required': 'final_test is required'})    
